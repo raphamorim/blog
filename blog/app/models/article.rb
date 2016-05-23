91374bc8b58da5b6263e35c1a175aff413cce816
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-05-23 20:10:16 -0300

    Added method for search on articles

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -3,4 +3,12 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
+
+  def self.search(search)
+    if search
+      self.where('title LIKE ?', "%#{search}%")
+    else
+      find(:all)
+    end
+  end
 end
