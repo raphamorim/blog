c3cba5c4d0f7fb5e2addad247f3567e3e10fc86f
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-05-29 18:31:06 -0300

    Database migration

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525221521) do
+ActiveRecord::Schema.define(version: 20160529213003) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160525221521) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "permalink"
+    t.text     "abstract"
   end
 
   add_index "articles", ["permalink"], name: "index_articles_on_permalink", unique: true
