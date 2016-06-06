f0ac3082f1efa47c29a81fac9ca42e5bf6043134
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-06-06 00:53:05 -0300

    Removed model ParagraphBlock

--- blog/app/models/paragraph_block.rb
+++
@@ -1,3 +0,0 @@
-class ParagraphBlock < ActiveRecord::Base
-  belongs_to :article
-end
--- blog/test/models/paragraph_block_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ParagraphBlockTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
