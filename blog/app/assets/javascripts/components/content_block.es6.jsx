3ab74896da20772e3745c86ea9b054cfb84de619
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-06-08 13:27:45 -0300

    Created onClick event for each content block type

--- blog/app/assets/javascripts/components/content_block.es6.jsx
+++ blog/app/assets/javascripts/components/content_block.es6.jsx
@@ -1,13 +1,19 @@
 class ContentBlock extends React.Component {
+
+  itemClick (event) {
+
+    console.log(event.currentTarget.parentNode.className);
+  }
+
   render () {
     return (
       <section className="content-blocks">
         <div className="article-content-type" >
             <ul>
-                <li className="video"><a></a></li>
-                <li className="photo"><a></a></li>
-                <li className="code"><a></a></li>
-                <li className="paragraph"><a></a></li>
+                <li className="video"><a href="#" onClick={this.itemClick}></a></li>
+                <li className="photo"><a href="#" onClick={this.itemClick}></a></li>
+                <li className="code"><a href="#" onClick={this.itemClick}></a></li>
+                <li className="paragraph"><a href="#" onClick={this.itemClick}></a></li>
             </ul>
         </div>
       </section>
