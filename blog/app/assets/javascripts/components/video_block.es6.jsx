6a22b79c2319a09cade1299b2cd00035462493a2
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-06-09 00:40:04 -0300

    Added control of edit and publish on render

--- blog/app/assets/javascripts/components/video_block.es6.jsx
+++ blog/app/assets/javascripts/components/video_block.es6.jsx
@@ -2,7 +2,8 @@ class VideoBlock extends React.Component {
 
   getInitialState () {
     return {
-        visible: false
+        visible: false,
+        published: false
     };
   }
 
@@ -17,13 +18,20 @@ class VideoBlock extends React.Component {
             <input type="text" />
         </p>
     );
-    return (
+
+    var video = (
         <div className="video-block">
             <video width="640" height="480" controls>
                 <source src="{this.props.url}" ></source>
             </video>
         </div>
     );
+
+    if (this.state.published) {
+        return video;
+    } else {
+        return form;
+    }
   }
 }
 
