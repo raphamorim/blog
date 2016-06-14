5de75d2333922161588ce06af92a44a9693f7bf0
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-06-13 23:39:47 -0300

    Text area allows to use tab during edition

--- blog/app/assets/javascripts/components/code_block.es6.jsx
+++ blog/app/assets/javascripts/components/code_block.es6.jsx
@@ -13,17 +13,22 @@ class CodeBlock extends React.Component {
 
   indent (event) {
 
-    if(event.keyCode == this.TAB) {
+    const key = event.keyCode;
+
+    if(key == this.TAB) {
 
         const value = event.target.value;
         const start = value.substring(0, event.target.selectionStart);
         const end = value.substring(event.target.selectionEnd);
 
-        this.setState({value: start + "\t" + end});
-        event.target.selectionStart = start + 1;
-    } else {
-        this.setState({value: event.target.value + event.keyCode});
+        this.setState({value: start + "    " + end});
+        event.target.selectionStart = start + 4;
+        event.preventDefault();
     }
+  }
+
+  change (event) {
+    this.setState({value: event.target.value});
   }
 
   render () {
@@ -31,7 +36,11 @@ class CodeBlock extends React.Component {
       <p className="code-block-form">
         <BlockRemove class="code-block-remove" clickHandler={this.props.removeCallback}/>
         <label>Source Code</label>
-        <textarea rows="15" cols="80" onKeyDown={this.indent.bind(this)} value={this.state.value}>
+        <textarea rows="15"
+                  cols="80"
+                  onKeyDown={this.indent.bind(this)}
+                  onChange={this.change.bind(this)}
+                  value={this.state.value}>
         </textarea>
       </p>
     );
