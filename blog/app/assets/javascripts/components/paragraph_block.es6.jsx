336d626533c2ebb8c1a9d72b4ffc5db40e823b0c
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-06-14 00:00:40 -0300

    Adding shortcuts control

--- blog/app/assets/javascripts/components/paragraph_block.es6.jsx
+++ blog/app/assets/javascripts/components/paragraph_block.es6.jsx
@@ -4,12 +4,29 @@ class ParagraphBlock extends React.Component {
       super();
       this.state = {
         visible: false,
-        published: false
+        published: false,
+        ctrlPressed: false
       }
+
+      this.CTRL = 17;
+      this.B = 66;
   }
 
-  destroy () {
-    debugger;
+  handleKeyDown (event) {
+
+    const key = event.keyCode;
+    if(key == this.CTRL) {
+        this.state.ctrlPressed = true;
+        console.log("ctrl pressed");
+    }
+  }
+
+  handleKeyUp (event) {
+
+    const key = event.keyCode;
+    if(key == this.B && this.state.ctrlPressed) {
+        debugger;
+    }
   }
 
   render () {
@@ -18,12 +35,16 @@ class ParagraphBlock extends React.Component {
       <p className="paragraph-block-form">
         <BlockRemove class="paragraph-block-remove" clickHandler={this.props.removeCallback}/>
         <label>Paragraph</label>
-        <textarea rows="5" columns="60"></textarea>
+        <textarea rows="5"
+                  columns="60"
+                  onKeyDown={this.handleKeyDown.bind(this)}
+                  onKeyUp={this.handleKeyUp.bind(this)}>
+        </textarea>
       </p>
     );
 
     const paragraph = (
-      <p>{this.props.text}</p>
+      <p >{this.props.text}</p>
     );
 
     if(this.state.published) {
