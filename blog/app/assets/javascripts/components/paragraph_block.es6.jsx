8fab14d71e21a72ff1bfd9b42b4d0e8ba76df406
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-06-15 00:18:18 -0300

    Added control for adding bold and italic on paragraph

--- blog/app/assets/javascripts/components/paragraph_block.es6.jsx
+++ blog/app/assets/javascripts/components/paragraph_block.es6.jsx
@@ -2,14 +2,34 @@ class ParagraphBlock extends React.Component {
 
   constructor () {
       super();
+
+      this.styles = {
+        NONE: 0,
+        BOLD: 1,
+        ITALIC: 2
+      }
+
       this.state = {
         visible: false,
         published: false,
-        ctrlPressed: false
+        ctrlPressed: false,
+        textStyle: this.styles.NONE,
+        value: ""
       }
 
       this.CTRL = 17;
       this.B = 66;
+      this.I = 73;
+
+      this.boldTags = {
+        OPEN: "<b>",
+        CLOSE: "</b>"
+      }
+
+      this.italicTags = {
+        OPEN: "<i>",
+        CLOSE: "</i>"
+      }
   }
 
   handleKeyDown (event) {
@@ -24,9 +44,60 @@ class ParagraphBlock extends React.Component {
   handleKeyUp (event) {
 
     const key = event.keyCode;
+    const start = event.target.selectionStart;
+    const end = event.target.selectionEnd;
+
     if(key == this.B && this.state.ctrlPressed) {
-        debugger;
+
+      this.setStyle(start, end, event, this.boldTags);
+    } else if(key == this.I && this.state.ctrlPressed) {
+
+      this.setStyle(start, end, event, this.italicTags);
+    } else if (key == this.CTRL) {
+      this.state.ctrlPressed = false;
     }
+  }
+
+  setStyle (start, end, event, currentTag) {
+
+    let tag = currentTag.OPEN;
+    let newStyle = this.styles.BOLD;
+    let newValue = this.state.value;
+    let length = 3;
+
+    if(this.state.textStyle == this.styles.BOLD) { // TODO: use italic too
+      tag = currentTag.CLOSE;
+      newStyle = this.styles.NONE;
+      length = 4;
+    }
+
+    if(start == end) {
+      newValue += tag;
+      if(end < this.state.value.length) {
+        newValue = this.state.value.substring(0, start) + tag + this.state.value.substring(end);
+        length = tag.length;
+      }
+    } else {
+      const before = this.state.value.substring(0, start) + tag;
+      const current = this.state.value.substring(start, end);
+      let after = this.state.value.substring(end) + currentTag.CLOSE;
+      if(end < this.state.value.length)
+        after = currentTag.CLOSE + this.state.value.substring(end);
+      newValue = before + current + after;
+      newStyle = this.styles.NONE;
+    }
+
+    this.setState({
+      value: newValue,
+      textStyle: newStyle
+    });
+
+    event.target.selectionEnd += length;
+    event.preventDefault();
+  }
+
+  change (event) {
+    this.setState({value: event.target.value});
   }
 
   render () {
@@ -38,7 +109,9 @@ class ParagraphBlock extends React.Component {
         <textarea rows="5"
                   columns="60"
                   onKeyDown={this.handleKeyDown.bind(this)}
-                  onKeyUp={this.handleKeyUp.bind(this)}>
+                  onKeyUp={this.handleKeyUp.bind(this)}
+                  onChange={this.change.bind(this)}
+                  value={this.state.value}>
         </textarea>
       </p>
     );
