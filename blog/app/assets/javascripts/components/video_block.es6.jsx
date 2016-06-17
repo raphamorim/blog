d45f7bb62b3d23837f19f46b580c8c406320e179
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-06-17 00:32:20 -0300

    Created the onChange callback to handle video url

--- blog/app/assets/javascripts/components/video_block.es6.jsx
+++ blog/app/assets/javascripts/components/video_block.es6.jsx
@@ -4,16 +4,52 @@ class VideoBlock extends React.Component {
       super();
       this.state = {
         visible: false,
-        published: false
+        published: false,
+        value: "",
+        videoID: undefined,
+      }
+
+      this.youtube = {
+        IMG_URL: "http://img.youtube.com/vi/",
+        IMG_QUALITY: "/0.jpg",
+        URL_PATTERN: /v=\w{11}/,
+        ID_LENGTH: 11,
       }
   }
 
+  handleKeyUp (event) {
+
+    const value = event.target.value;
+    let videoID = undefined;
+
+    if(this.youtube.URL_PATTERN.test(value)) {
+        videoID = value.split("v=")[1].substring(0, this.youtube.ID_LENGTH);
+    }
+    this.setState({value: value, videoID: videoID});
+  }
+
   render () {
+    let image = "";
+    if(this.state.videoID) {
+      const videoThumbURL = this.youtube.IMG_URL + this.state.videoID
+                            + this.youtube.IMG_QUALITY;
+      image = (
+        <figure className="video-thumb">
+        <img src={videoThumbURL}/>
+        </figure>
+      );
+    }
+
     const form = (
         <p className="video-block-form">
+          <section>
             <label>Video url</label>
-            <input type="text" />
+            <input type="text"
+                   onChange={this.handleKeyUp.bind(this)}
+                   value={this.state.value}/>
             <BlockRemove class="video-block-remove" clickHandler={this.props.removeCallback}/>
+          </section>
+          {image}
         </p>
     );
 
