14af099e80c3340128cedc8c95a41c5bc253df74
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-07-04 23:16:33 -0300

    Updated database schema

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606015939) do
+ActiveRecord::Schema.define(version: 20160705020018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,14 @@ ActiveRecord::Schema.define(version: 20160606015939) do
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
