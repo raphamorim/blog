ae98486f3bc662c75c3187ec131ead560975bbb0
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-07-20 13:30:50 -0300

    Added space at create method

--- blog/app/controllers/sessions_controller.rb
+++ blog/app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.authenticate(params[:email], params[:password])
+
     if user
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Logged in!"
