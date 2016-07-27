a7f0dccd829c65faec632009a187dced815fa46f
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-07-27 00:12:02 -0300

    The controller verifies the login for cover upload and redirects to articles_path

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
-
-  before_action :require_login, only: [:edit, :update, :create, :new, :destroy]
+  before_action :require_login, only: [:edit, :update,
+                                       :create, :new, :destroy, :upload_cover]
+  skip_before_filter :verify_authenticity_token, :only => :upload_cover
 
   def index
     @articles = Article.all
@@ -51,6 +51,27 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def upload_cover()
+
+    if params[:cover]
+      @article = Article.find_by_permalink(params[:id])
+
+      path_prefix = "uploads"
+      uploaded_io = params[:cover]
+      name = uploaded_io.original_filename
+      digest = Digest::SHA1.hexdigest("#{name}-{Time.now.to_i}")
+      path = Rails.root.join('public', path_prefix, digest)
+
+      File.open(path, 'wb') do |file|
+        file.write(uploaded_io.read)
+      end
+
+      @article.cover = "/" + path_prefix + "/" + digest
+      @article.save
+    end
+    redirect_to articles_path
+  end
+
   private
 
     def require_login
@@ -65,7 +86,7 @@ class ArticlesController < ApplicationController
       params[:article][:blocks] = normalize_params
 
       params.require(:article).permit(
-        :title, :subtitle, :abstract, :text, :all_tags, blocks: [:paragraph, :video, :photo, :code]
+        :title, :subtitle, :abstract, :text, :cover, :all_tags, blocks: [:paragraph, :video, :photo, :code]
       )
     end
 
@@ -93,28 +114,6 @@ class ArticlesController < ApplicationController
         end
 
         params[:article][:blocks][:photo] = "/" + path_prefix + "/" + digest
-      end
-    end
-
-    def upload_cover()
-
-      if params[:cover]
-        @article = Article.find_by_permalink(params[:id])
-
-        path_prefix = "uploads"
-        uploaded_io = params[:cover]
-        name = uploaded_io.original_filename
-        digest = Digest::SHA1.hexdigest("#{name}-{Time.now.to_i}")
-        path = Rails.root.join('public', path_prefix, digest)
-
-        File.open(path, 'wb') do |file|
-          file.write(uploaded_io.read)
-        end
-
-        @article.cover = "/" + path_prefix + "/" + digest
-        @article.save
-
-        redirect_to "index"
       end
     end
 end
