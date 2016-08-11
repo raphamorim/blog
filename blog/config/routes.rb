50f4dd755ab3c3ccba4bc93f98ff0d14b7fbeb35
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-08-11 20:15:29 -0300

    Adding route to RSS feed

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'article/index'
 
+  get 'feed' => 'articles#feed'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
