9cd981bad337a0a02d9d2db77af08c2eb53c9d94
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-08-11 18:00:44 -0300

    Top articles are requested on analytics and cached for a day in Redis

--- blog/app/helpers/home_helper.rb
+++ blog/app/helpers/home_helper.rb
@@ -3,7 +3,11 @@ require 'googleauth'
 
 module HomeHelper
 
-  def get_top_articles
+  TOP_ARTICLES_KEY = "top_articles"
+  IN_A_DAY = 60 * 60 * 24
+
+  def request_analytics_top_articles()
+
     scopes = ["https://www.googleapis.com/auth/analytics.readonly"]
     authorization = Google::Auth.get_application_default(scopes)
     authorization.fetch_access_token!
@@ -21,13 +25,32 @@ module HomeHelper
       :dimensions => 'ga:pageTitle,ga:pagePath',
       :sort => '-ga:pageviews'
     )
+    result
+  end
+
+  def get_top_articles
 
     top_articles = []
-    result.rows.each do |row|
-      if row[1].include? "/articles/" and not row[1].include? "/articles/new"
-        article = Article.find_by_permalink(row[1].split("/articles/")[-1].split("/")[0])
-        top_articles.append(article)
+
+    if $redis.exists(TOP_ARTICLES_KEY)
+      articles = $redis.smembers(TOP_ARTICLES_KEY)
+      articles.each do |json_article|
+        article = Article.new
+        top_articles.append(article.from_json(json_article))
       end
+
+    else
+      result = request_analytics_top_articles()
+      result.rows.each do |row|
+        if row[1].include? "/articles/" and not row[1].include? "/articles/new"
+          article = Article.find_by_permalink(row[1].split("/articles/")[-1].split("/")[0])
+          if article
+            top_articles.append(article)
+            $redis.sadd(TOP_ARTICLES_KEY, article.to_json)
+          end
+        end
+      end
+      $redis.expire(TOP_ARTICLES_KEY, IN_A_DAY)
     end
 
     top_articles
