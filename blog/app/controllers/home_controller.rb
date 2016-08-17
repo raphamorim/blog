b3f466e75f27899eb2ba0fd199062c974d9fc659
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-08-17 18:08:28 -0300

    Started using the recommendation helper

--- blog/app/controllers/home_controller.rb
+++ blog/app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
   include HomeHelper
+  include UsersHelper
 
   def index
     @last_articles = Article.first(4)
     @top_articles = get_top_articles()
+    @recommended = recommendation()
   end
 end
