af4b25817141698a77c10b8c80b1420fd581bbda
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-08-17 18:08:02 -0300

    Creating methods to recommend articles per region

--- blog/app/helpers/users_helper.rb
+++ blog/app/helpers/users_helper.rb
@@ -1,2 +1,25 @@
+require 'net/http'
+require 'json'
+
 module UsersHelper
+
+  DEFAULT_STATE = "Minas Gerais"
+
+  def get_state_from_request()
+
+    REGION_API = "http://localhost:8080/json/"
+
+    url = URI.parse(REGION_API + request.remote_ip
+    res = Net::HTTP.get(url)
+    json = JSON.parse(res)
+    # return json region field
+  end
+
+  def recommendation()
+
+    # get state. Otherwise default
+    # Check cache of state. Otherwise request from analytics
+    # if necessary, cache it and return
+    binding.pry
+  end
 end
