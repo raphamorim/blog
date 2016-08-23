137152704b9799228565758301ab508b4dd06309
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-08-22 21:01:46 -0300

    Recommendation is made by country and region of the request

--- blog/app/helpers/users_helper.rb
+++ blog/app/helpers/users_helper.rb
@@ -7,21 +7,21 @@ require 'googleauth'
 module UsersHelper
 
   IN_A_DAY = 60 * 60 * 24
-  DEFAULT_LOCATION = "BR-RJ"
+  DEFAULT_LOCATION = "Brazil-Rio de Janeiro"
+  REGION_API = "http://localhost:8080/json/"
 
   def get_location_from_request()
 
-    REGION_API = "http://localhost:8080/json/"
 
-    url = URI.parse(REGION_API + request.remote_ip
+    url = URI.parse(REGION_API + request.remote_ip)
     res = Net::HTTP.get(url)
     json = JSON.parse(res)
 
-    location = json[:country_code] + "-" + json[:region_code]
-    if location == "-"
+    if json[:country_name].nil? or json[:region_name].nil?
       return DEFAULT_LOCATION
     end
-    return location
+
+    json[:country_name] + "-" + json[:region_name]
   end
 
   def recommendation()
@@ -36,12 +36,12 @@ module UsersHelper
         recommended.append(article.from_json(json_article))
       end
     else
-      result = request_analytics_by_location()
+      result = request_analytics_by_location(location)
       result.rows.each do |row|
         if row[1].include? "/articles/" and not row[1].include? "/articles/new"
           article = Article.find_by_permalink(row[1].split("/articles/")[-1].split("/")[0])
           if article
-            top_articles.append(article)
+            recommended.append(article)
             $redis.sadd(location, article.to_json)
           end
         end
@@ -51,24 +51,28 @@ module UsersHelper
     recommended
   end
 
-  def request_analytics_by_location()
+  def request_analytics_by_location(location)
     scopes = ["https://www.googleapis.com/auth/analytics.readonly"]
     authorization = Google::Auth.get_application_default(scopes)
     authorization.fetch_access_token!
 
     analytics = Google::Apis::AnalyticsV3::AnalyticsService.new
     analytics.authorization = authorization
-    @startDate = DateTime.now.prev_month.strftime("%Y-%m-%d")
-    @endDate = DateTime.now.strftime("%Y-%m-%d")
+    startDate = DateTime.now.prev_month.strftime("%Y-%m-%d")
+    endDate = DateTime.now.strftime("%Y-%m-%d")
+    country = location.split("-")[0]
+    region = location.split("-")[1]
 
     result = analytics.get_ga_data(
       ids="ga:126632208",
-      start_date=@startDate,
-      end_date=@endDate,
+      start_date=startDate,
+      end_date=endDate,
       metrics='ga:pageviews',
-      :dimensions => 'ga:pageTitle,ga:pagePath', # TODO: Change dimension to region segment
-      :sort => '-ga:pageviews'
+      :dimensions => 'ga:pageTitle,ga:pagePath',
+      :sort => '-ga:pageviews',
+      :filters => "ga:country==#{country},ga:region==#{region}"
     )
+
     result
   end
 end
