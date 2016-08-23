27b90ee2c8df5c427b3cbca23f52107586dc9750
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-08-23 13:00:16 -0300

    Running migration for new model Talks

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726003005) do
+ActiveRecord::Schema.define(version: 20160823155939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,17 @@ ActiveRecord::Schema.define(version: 20160726003005) do
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "talks", force: :cascade do |t|
+    t.string   "title"
+    t.string   "conf_name"
+    t.string   "conf_link"
+    t.date     "conf_date"
+    t.string   "slides"
+    t.string   "video"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
