277154f19c8318b547b29482ed328b62ab506c0a
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-09-28 14:18:32 -0300

    Started using contenteditable for controlling rich text editor

--- blog/app/assets/javascripts/components/paragraph_block.es6.jsx
+++ blog/app/assets/javascripts/components/paragraph_block.es6.jsx
@@ -7,43 +7,15 @@ class ParagraphBlock extends React.Component {
   constructor (props) {
     super(props);
 
-    this.styles = {
-      NONE: 0,
-      BOLD: 1,
-      ITALIC: 2
-    }
-
     this.state = {
       published: this.props.published || false,
       ctrlPressed: false,
-
-      textStyle: this.styles.NONE,
       value: this.props.text || ""
     }
 
-    /* Key Codes */
     this.CTRL = 17;
-    this.B = 66;
-    this.I = 73;
-    this.K = 75;
-
-    /* Tag bold symbols */
-    this.boldTags = {
-      OPEN: "<b>",
-      CLOSE: "</b>"
-    }
-
-    /* Tag italic symbols */
-    this.italicTags = {
-      OPEN: "<i>",
-      CLOSE: "</i>"
-    }
-
-    /* Link symbol */
-    this.linkTags = {
-      OPEN: "<a href='' >",
-      CLOSE: "</a>"
-    }
+    this.CMD = 91;
+    this.L = 76;
   }
 
   /**
@@ -53,7 +25,7 @@ class ParagraphBlock extends React.Component {
 
     const key = event.keyCode;
 
-    if(key == this.CTRL) {
+    if(key == this.CTRL || key == this.CMD) {
       this.state.ctrlPressed = true;
     }
   }
@@ -65,130 +37,44 @@ class ParagraphBlock extends React.Component {
    */
   handleKeyUp (event) {
 
+    event.preventDefault();
+
     const key = event.keyCode;
     const selection = window.getSelection();
     const start = selection.anchorOffset;
     const end = selection.focusOffset;
 
-    if(key == this.B && this.state.ctrlPressed) {
-      this.setStyle(start, end, selection, this.boldTags);
-
-    } else if(key == this.I && this.state.ctrlPressed) {
-      this.setStyle(start, end, selection, this.italicTags);
-
-    } else if(key == this.K && this.state.ctrlPressed) {
-      this.addLink(start, end, selection, this.linkTags);
-    } else if (key == this.CTRL) {
+    if(key == this.L && this.state.ctrlPressed) {
+      this.addLink();
+    } else if (key == this.CTRL || key == this.CMD) {
       this.state.ctrlPressed = false;
     }
-
-    event.preventDefault();
   }
 
-  /**
-   * Verifies the current style and sets new one
-   */
-  checkStyle (data, tagType) {
-
-    const current = this.state.textStyle;
-
-    if(current == this.styles.BOLD || current == this.styles.ITALIC) {
-      data.tag = tagType.CLOSE;
-      data.newStyle = this.styles.NONE;
-      data.length = 4;
-    }
-  }
-
-  /**
-   * Open or close tags at cursor place
-   */
-  openOrCloseTag(data, start, end) {
-
-    /* Appends tag on text */
-    data.newValue += data.tag;
-
-    /* Inserts tag in the middle of a text */
-    if(end < this.state.value.length) {
-
-      const before = this.state.value.substring(0, start);
-      const after = this.state.value.substring(end);
-
-      data.newValue = before + data.tag + after;
-      data.length = data.tag.length;
-    }
-  }
-
-  /**
-   * Wraps selected text with the tag type (bold or italic)
-   */
-  wrapSelectionWithTag (data, start, end, tagType) {
-
-    const before = this.state.value.substring(0, start) + data.tag;
-    const current = this.state.value.substring(start, end);
-    let after = this.state.value.substring(end) + tagType.CLOSE;
-
-    if(end < this.state.value.length)
-      data.after = tagType.CLOSE + this.state.value.substring(end);
-
-    data.newValue = before + current + after;
-    data.newStyle = this.styles.NONE;
-  }
-
-  /**
-   * Sets the style of the current selection or open/close bold or italic tags
-   */
-  setStyle (start, end, selection, tagType) {
-
-    let data = {
-      tag: tagType.OPEN,
-      newStyle: (tagType==this.boldTags)? this.styles.BOLD: this.styles.ITALIC,
-      newValue: this.state.value,
-      length: 3,
-    };
-
-    this.checkStyle(data, tagType);
-
-    if(start == end) {
-      this.openOrCloseTag(data, start, end);
-
-    } else {
-      this.wrapSelectionWithTag(data, start, end, tagType);
-    }
-
-    this.setState({
-      value: data.newValue,
-      textStyle: data.newStyle
-    });
-
-    this.setPosition(selection, start, end);
-  }
-
-  setPosition (selection, start, end) {
-
-    let range = document.createRange()
-    debugger;
-  }
 
   /**
    * Adds links at start and end text selection
    */
-  addLink (start, end, event, tagType) {
+  addLink () {
 
-    const before = this.state.value.substring(0, start) + tagType.OPEN;
-    const current = this.state.value.substring(start, end);
-    let after = this.state.value.substring(end) + tagType.CLOSE;
-
-    const newValue = before + current + after;
-    this.setState({
-      value: newValue
-    });
-
-    event.target.selectionEnd += data.length;
+    if (window.getSelection().toString()) {
+        var a = document.createElement('a');
+            a.href = 'https://pantuza.com';
+            window.getSelection().getRangeAt(0).surroundContents(a);
+    }
     event.preventDefault();
   }
 
   change (event) {
     this.setState({value: event.target.value});
+  }
+
+  allowEdition (event) {
+    event.target.contentEditable = true;
+  }
+
+  closeEdition (event) {
+    event.target.contentEditable = false;
   }
 
   render () {
@@ -198,11 +84,12 @@ class ParagraphBlock extends React.Component {
         <BlockRemove class="paragraph-block-remove" clickHandler={this.props.removeCallback}/>
         <label>Paragraph</label>
         <div
-          contentEditable="true"
           className="unpublished"
           onKeyDown={this.handleKeyDown.bind(this)}
           onKeyUp={this.handleKeyUp.bind(this)}
           onChange={this.change.bind(this)}
+          onClick={this.allowEdition.bind(this)}
+          onBlur={this.closeEdition.bind(this)}
           name={"article[blocks][" + this.props.order + "][paragraph]"}>
           {this.state.value}
         </div>
