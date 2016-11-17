da85b7f678fb0d7adaeda15f13204e9c9530397a
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-11-17 18:35:46 -0200

    Created methods to handle robots.txt and sitemap.xml requests

--- blog/app/controllers/static_controller.rb
+++ blog/app/controllers/static_controller.rb
@@ -4,4 +4,17 @@ class StaticController < ApplicationController
     path = request.env['PATH_INFO']
     render :layout => false, template: 'static' + path
   end
+
+  def robots
+    render :layout => false
+  end
+
+  def sitemap
+    path = Rails.root.join("public", "sitemaps", "sitemap.xml")
+    if File.exists?(path)
+      render xml: open(path).read
+    else
+      render text: "Sitemap not found.", status: :not_found
+    end
+  end
 end
