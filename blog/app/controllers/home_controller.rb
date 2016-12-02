f38c899d8839e434921e4ff674985b02b436cc53
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-12-02 09:20:31 -0200

    Getting the latest articles first

--- blog/app/controllers/home_controller.rb
+++ blog/app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   include UsersHelper
 
   def index
-    @last_articles = Article.last(8)
+    @last_articles = Article.first(8)
     @top_articles = get_top_articles()
     @recommended = recommendation()[0, 4]
   end
