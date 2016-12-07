6851388d3361f14da13ca42a84f2d5bdf5f67981
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-12-07 15:33:06 -0200

    Always bring articles ordered by id desc

--- blog/app/controllers/home_controller.rb
+++ blog/app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   include UsersHelper
 
   def index
-    @last_articles = Article.first(8)
+    @last_articles = Article.order('id DESC').last(8)
     @top_articles = get_top_articles()
     @recommended = recommendation()[0, 4]
   end
