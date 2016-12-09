6884ffef5d138934b576b1a45004eea6dc66e8ef
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-12-09 13:13:12 -0200

    Created method to help getting an article image for share bar

--- blog/app/helpers/articles_helper.rb
+++ blog/app/helpers/articles_helper.rb
@@ -1,2 +1,12 @@
 module ArticlesHelper
+
+  def get_share_image(article)
+
+    @article.blocks.each do |key, hash|
+      if hash.key?("photo")
+        return root_url[0, root_url.size - 1] + hash["photo"]
+      end
+    end
+
+  end
 end
