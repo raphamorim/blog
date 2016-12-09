c116b27bf417b35caa35a5dfd9134417f26ac0ee
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-12-09 13:19:27 -0200

    BugFix: Type casting hash to string

--- blog/app/helpers/articles_helper.rb
+++ blog/app/helpers/articles_helper.rb
@@ -2,11 +2,13 @@ module ArticlesHelper
 
   def get_share_image(article)
 
-    @article.blocks.each do |key, hash|
-      if hash.key?("photo")
-        return root_url[0, root_url.size - 1] + hash["photo"]
+    if @article.blocks
+      @article.blocks.each do |key, hash|
+        if hash.key?("photo")
+          return root_url[0, root_url.size - 1] + hash["photo"]["path"]
+        end
       end
     end
-
+    return ""
   end
 end
